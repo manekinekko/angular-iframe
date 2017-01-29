3e9e6383f049415b77f055237c02552f9949c702
Wassim CHEGHAM <github@wassimchegham.com>
2017-01-29 01:08:21 +0100

    feat: 😎

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# App1
+# Angular Mini-Browser
 
 This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.26.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  unsafeIframeUrl = 'https://wassimchegham.com';
+  safeIframeUrl = '#';
+
+  constructor(public ds: DomSanitizer) {
+  }
+
+  ngOnInit() {
+    this.setIframeUrl();
+  }
+
+  setIframeUrl() {
+    this.safeIframeUrl = this.ds.bypassSecurityTrustResourceUrl(this.unsafeIframeUrl) as string;
+  }
+
 }
